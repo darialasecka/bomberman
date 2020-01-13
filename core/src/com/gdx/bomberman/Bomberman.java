56610dae6f452d402b743f80f7f0040daaaacb33
Daria Lasecka <daria.las@wp.pl>
2020-01-13 23:08:44 +0100

    Players now see when others players move

--- core/src/com/gdx/bomberman/Bomberman.java
+++ core/src/com/gdx/bomberman/Bomberman.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 import com.gdx.bomberman.sprites.Bomber;
 import io.socket.client.IO;
@@ -18,8 +19,9 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 
-
 public class Bomberman extends ApplicationAdapter {
+	private final float UPDATE_TIME = 1/60f;
+	float timer;
 	SpriteBatch batch;
 	private Socket socket;
 	String id;
@@ -45,16 +47,37 @@ public class Bomberman extends ApplicationAdapter {
 			} else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
 				player.setPosition(player.getX() + (+200 * dt), player.getY());
 			}
+			if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+				player.setPosition(player.getX(), player.getY() + (+200 * dt));
+			} else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+				player.setPosition(player.getX(), player.getY()+ (-200 * dt));
+			}
 
+		}
+	}
+
+	public void updateServer(float dt){
+		timer += dt;
+		if(timer >= UPDATE_TIME && player != null && player.hasMoved()){
+			JSONObject data = new JSONObject();
+			try{
+				data.put("x", player.getX());
+				data.put("y", player.getY());
+				socket.emit("playerMoved", data);
+
+			} catch (JSONException e) {
+				Gdx.app.log("SocketIO", "Error sending update data");
+			}
 		}
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		handleInput(Gdx.graphics.getDeltaTime());
+		updateServer(Gdx.graphics.getDeltaTime());
 
 		batch.begin();
 		if(player != null){
@@ -104,11 +127,55 @@ public class Bomberman extends ApplicationAdapter {
 			public void call(Object... args) {
 				JSONObject data = (JSONObject) args[0];
 				try {
-					String id = data.getString("id");
-					Gdx.app.log("SocketIO", "New Player Connected: " + id);
-					otherPlayers.put(id, new Bomber(otherBomber));
+					String playerId = data.getString("id");
+					Gdx.app.log("SocketIO", "New Player Connected: " + playerId);
+					otherPlayers.put(playerId, new Bomber(otherBomber));
 				} catch (JSONException e) {
-					Gdx.app.log("SocketIO", "Error getting New Player ID");
+					Gdx.app.log("SocketIO", "Error getting New PlayerID");
+				}
+			}
+		}).on("playerDisconnected", new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				JSONObject data = (JSONObject) args[0];
+				try {
+					String playerId = data.getString("id");
+					otherPlayers.remove(playerId);
+				} catch (JSONException e) {
+					Gdx.app.log("SocketIO", "Error getting disconnected PlayerID");
+				}
+			}
+		}).on("getPlayers", new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				JSONArray objects = (JSONArray) args[0];
+				try {
+					for(int i = 0; i < objects.length(); i++){
+						Bomber enemyPlayer = new Bomber((otherBomber));
+						Vector2 position = new Vector2();
+						position.x = ((Double) objects.getJSONObject(i).getDouble("x")).floatValue();
+						position.y = ((Double) objects.getJSONObject(i).getDouble("y")).floatValue();
+						enemyPlayer.setPosition(position.x, position.y);
+
+						otherPlayers.put(objects.getJSONObject(i).getString("id"), enemyPlayer);
+					}
+				} catch (JSONException e) {
+					Gdx.app.log("SocketIO", "Error getting other players");
+				}
+			}
+		}).on("playerMoved", new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				JSONObject data = (JSONObject) args[0];
+				try {
+					String playerId = data.getString("id");
+					Double x = data.getDouble("x");
+					Double y = data.getDouble("y");
+					if(otherPlayers.get(playerId) != null){
+						otherPlayers.get(playerId).setPosition(x.floatValue(), y.floatValue());
+					}
+				} catch (JSONException e) {
+					Gdx.app.log("SocketIO", "Error getting Player coordinates");
 				}
 			}
 		});

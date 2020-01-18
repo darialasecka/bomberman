3ee1336350b92d994fc414c5d1be67059fddc3d6
Daria Lasecka <daria.las@wp.pl>
2020-01-18 16:15:00 +0100

    Moved map implementation to Room

--- server/Server.java
+++ server/Server.java
@@ -32,31 +32,9 @@ class Room {
 	public final int MAX_CAPACITY = 4;
 	public final int MIN_CAPACITY_REQUIRED = 2;
 
-	public Room(int number) throws IOException {
-		this.number = number;
-		this.active_game = false;
-		this.is_full = false;
-	}
 
-	public void broadcast(String msg) {
-		for(Player player: players_in_room) {
-			try {
-				synchronized (player.out){
-					player.out.writeUTF(msg);
-				}
-			} catch (IOException e) {
-				//e.printStackTrace();
-				System.out.println("Failed to broadcast to client.");
-				curr_number_of_players--;
-				players_in_room.remove(player);
-			}
-		}
-	}
-}
-
-class Multi extends Thread {
-	private Player player;
 	float blockSize = 32;
+
 	public ArrayList<ArrayList<String>> map = new ArrayList<>();
 
 	ArrayList walls;
@@ -74,12 +52,14 @@ class Multi extends Thread {
 	ArrayList floor6;
 
 
-	Multi(Player player){
-		this.player = player;
+
+	public Room(int number) throws IOException {
+		this.number = number;
+		this.active_game = false;
+		this.is_full = false;
 
 		setMap();
 		setBoxes();
-
 	}
 
 	public void printMap(){
@@ -234,7 +214,6 @@ class Multi extends Thread {
 		map.get(7).set(15, box);
 		//line 8
 		map.get(8).set(1, box);
-		map.get(8).set(2, box);
 		map.get(8).set(5, box);
 		map.get(8).set(7, box);
 		map.get(8).set(11, box);
@@ -265,21 +244,34 @@ class Multi extends Thread {
 		map.get(11).set(12, box);
 	}
 
-	public void sendBoxesPositions(DataOutputStream out) throws IOException {
+	public void broadcast(String msg) {
+		for(Player player: players_in_room) {
+			try {
+				synchronized (player.out){
+					player.out.writeUTF(msg);
+				}
+			} catch (IOException e) {
+				//e.printStackTrace();
+				System.out.println("Failed to broadcast to client.");
+				curr_number_of_players--;
+				players_in_room.remove(player);
+			}
+		}
+	}
+
+	public void sendBoxesPositions() throws IOException {
 		for(int i = 1; i < map.size()-1; i++){
 			for(int j = 1; j < walls.size()-1; j++){ //którakolwiek
 				if (map.get(i).get(j) == "2"){
 					float boxX = (j * blockSize) - 0.5f;
 					float boxY = ((map.size() - 1.15f) * blockSize) - (i * blockSize);//chyba tak xd
-					synchronized (out) {
-						out.writeUTF("box " + Server.boxNumber++ + " " + boxX + " " + boxY);
-					}
+					broadcast("box " + Server.boxNumber++ + " " + boxX + " " + boxY);
 				}
 			}
 		}
 	}
 
-	public void sendBlastPositions(DataOutputStream out, String start, String bombNumber) throws IOException {
+	public void sendBlastPositions(String start, String bombNumber) throws IOException {
 		for(int i = 1; i < map.size()-1; i++){
 			for(int j = 1; j < walls.size()-1; j++){ //którakolwiek
 				if (map.get(i).get(j).startsWith("3")){
@@ -290,12 +282,18 @@ class Multi extends Thread {
 					if (position.endsWith("0") || position.endsWith("1")) type = "h";
 					else if (position.endsWith("2") || position.endsWith("3")) type = "v";
 					else type = "c";
-					synchronized (out) {
-						out.writeUTF("blast " + Server.blastNumber++ + " " + blastX + " " + blastY + " " + type + " " + start + " " + bombNumber);
-					}
+					broadcast("blast " + Server.blastNumber++ + " " + blastX + " " + blastY + " " + type + " " + start + " " + bombNumber);
 				}
 			}
 		}
+	}
+}
+
+class Multi extends Thread {
+	private Player player;
+
+	Multi(Player player){
+		this.player = player;
 	}
 
 	public void run()  {
@@ -309,34 +307,36 @@ class Multi extends Thread {
 				if (msg.startsWith("connected")) {
 					synchronized (player.out){
 						player.out.writeUTF("created " + player.id + " " + player.x + " " + player.y);
-						sendBoxesPositions(player.out);
+						Room room = Server.rooms.get(player.room);
+						room.sendBoxesPositions();
 					}
 				} else if (msg.startsWith("playerMoved")) {
 					String x = msg.split(" ")[1];
 					String y = msg.split(" ")[2];
 					int direction = Integer.parseInt(msg.split(" ")[3]);
-					int posX = (int)(player.x / blockSize);// tak chemy zaokrąglić w górę
-					int posY = (int)(player.y / blockSize);
-					posY = map.size()-posY - 1;
+
+					Room room = Server.rooms.get(player.room);
+					int posX = (int)(player.x / room.blockSize);// tak chemy zaokrąglić w górę
+					int posY = (int)(player.y / room.blockSize);
+					posY = room.map.size()-posY - 1;
 					//System.out.println(posX + " " + posY);
 
 					if(direction == 0){//lewo
 						//System.out.println(posX + " " + map.get(posY).get(posX));
-						if(map.get(posY).get(posX) == "0" || map.get(posY).get(posX).startsWith("3")) player.x = Float.parseFloat(x);
+						if(room.map.get(posY).get(posX) == "0" || room.map.get(posY).get(posX).startsWith("3")) player.x = Float.parseFloat(x);
 							/*System.out.println("Tak");
 						else System.out.println("Nie");*/
 					} else if(direction == 1) {//prawo
-						posX = (int)(((player.x) / blockSize) - 0.5);
-						if(map.get(posY).get(posX+1) == "0" || map.get(posY).get(posX+1).startsWith("3")) player.x = Float.parseFloat(x);
+						posX = (int)(((player.x) / room.blockSize) - 0.5);
+						if(room.map.get(posY).get(posX+1) == "0" || room.map.get(posY).get(posX+1).startsWith("3")) player.x = Float.parseFloat(x);
 					} else if(direction == 2){//góra
-						posY = (int)((player.y / blockSize) - 0.75);
-						posY = map.size()-posY-2;
-						if(map.get(posY).get(posX) == "0" || map.get(posY).get(posX).startsWith("3")) player.y = Float.parseFloat(y);
+						posY = (int)((player.y / room.blockSize) - 0.75);
+						posY = room.map.size()-posY-2;
+						if(room.map.get(posY).get(posX) == "0" || room.map.get(posY).get(posX).startsWith("3")) player.y = Float.parseFloat(y);
 					} else if(direction == 3) {//dól
-						if(map.get(posY).get(posX) == "0" || map.get(posY).get(posX).startsWith("3")) player.y = Float.parseFloat(y);
+						if(room.map.get(posY).get(posX) == "0" || room.map.get(posY).get(posX).startsWith("3")) player.y = Float.parseFloat(y);
 					}
 					//player.y = Float.parseFloat(y);
-					Room room = Server.rooms.get(player.room);
 					for (Player other : room.players_in_room) {
 						synchronized (player.out){
 							player.out.writeUTF("update " + other.id + " " + other.x + " " + other.y);
@@ -357,62 +357,67 @@ class Multi extends Thread {
 					//String power = "4";//do tesów
 
 					// tu ogarniamy pozycje bomby i liczymy gdzie powinien być wybuch
-					int posX = (int)(Float.parseFloat(x) / blockSize);
-					int posY = (int)(Float.parseFloat(y) / blockSize);
-					posY = map.size()-posY - 1;
+					int posX = (int)(Float.parseFloat(x) / room.blockSize);
+					int posY = (int)(Float.parseFloat(y) / room.blockSize);
+					posY = room.map.size()-posY - 1;
 
 					int count = 0;
 					//środek
-					map.get(posY).set(posX,"3"+bombNumber+"-");
+					room.map.get(posY).set(posX,"3"+bombNumber+"-");
 
 					//w lewo
-					while((map.get(posY).get(posX-count-1) == "0" || map.get(posY).get(posX-count-1).startsWith("3") || map.get(posY).get(posX-count-1) == "2") && count != Integer.parseInt(power)){
-						map.get(posY).set(posX-count-1, "3"+bombNumber+"0"); //lewo oznacza inaczej
+					while((room.map.get(posY).get(posX-count-1) == "0" || room.map.get(posY).get(posX-count-1).startsWith("3") || room.map.get(posY).get(posX-count-1) == "2") && count != Integer.parseInt(power)){
+						room.map.get(posY).set(posX-count-1, "3"+bombNumber+"0"); //lewo oznacza inaczej
 						count++;
 					}
 					//gora
 					count = 0;
-					while((map.get(posY-count-1).get(posX) == "0" || map.get(posY-count-1).get(posX).startsWith("3") || map.get(posY-count-1).get(posX) == "2") && count != Integer.parseInt(power)){
-						map.get(posY-count-1).set(posX, "3"+bombNumber+"2");
+					while((room.map.get(posY-count-1).get(posX) == "0" || room.map.get(posY-count-1).get(posX).startsWith("3") || room.map.get(posY-count-1).get(posX) == "2") && count != Integer.parseInt(power)){
+						room.map.get(posY-count-1).set(posX, "3"+bombNumber+"2");
 						count++;
 					}
 					//prawo
 					count = 0;
-					while((map.get(posY).get(posX+count+1) == "0" || map.get(posY).get(posX+count+1).startsWith("3") || map.get(posY).get(posX+count+1) == "2") && count != Integer.parseInt(power)){
-						map.get(posY).set(posX+count+1, "3"+bombNumber+"1");
+					while((room.map.get(posY).get(posX+count+1) == "0" || room.map.get(posY).get(posX+count+1).startsWith("3") || room.map.get(posY).get(posX+count+1) == "2") && count != Integer.parseInt(power)){
+						room.map.get(posY).set(posX+count+1, "3"+bombNumber+"1");
 						count++;
 					}
 					//dol
 					count = 0;
-					while((map.get(posY+count+1).get(posX) == "0" || map.get(posY+count+1).get(posX).startsWith("3") || map.get(posY+count+1).get(posX) == "2") && count != Integer.parseInt(power)){
-						map.get(posY+count+1).set(posX, "3"+bombNumber+"3");
+					while((room.map.get(posY+count+1).get(posX) == "0" || room.map.get(posY+count+1).get(posX).startsWith("3") || room.map.get(posY+count+1).get(posX) == "2") && count != Integer.parseInt(power)){
+						room.map.get(posY+count+1).set(posX, "3"+bombNumber+"3");
 						count++;
 					}
 					//wybuchy dobrze oznacza
 					//printMap();
-
-					sendBlastPositions(player.out, msg.split(" ")[5], bombNumber);
+					for (Player other : room.players_in_room) {
+						synchronized (player.out){
+							player.out.writeUTF("update " + other.id + " " + other.x + " " + other.y);
+						}
+					}
+					room.sendBlastPositions(msg.split(" ")[5], bombNumber);
 					room.broadcast("clearBoxes");
-					sendBoxesPositions(player.out);
+					room.sendBoxesPositions();
+
 				} else if(msg.startsWith("endBlast")){
 					Room room = Server.rooms.get(player.room);
 					room.broadcast(msg);
 
 					String bombNumber = msg.split(" ")[1];
 
-					for(int i = 1; i < map.size()-1; i++){
-						for(int j = 1; j < walls.size()-1; j++) {
+					for(int i = 1; i < room.map.size()-1; i++){
+						for(int j = 1; j < room.walls.size()-1; j++) {
 						//sprawdzić czy bombNumber jest ten sam i usunąć
-							String position = map.get(i).get(j);
+							String position = room.map.get(i).get(j);
 							if(position.length() >= 3){
 								String mapBombNumber = position.substring(1, position.length()-1);
-								System.out.println(mapBombNumber);
-								if(mapBombNumber.equals(bombNumber)) map.get(i).set(j, "0");
+								//System.out.println(mapBombNumber);
+								if(mapBombNumber.equals(bombNumber)) room.map.get(i).set(j, "0");
 							}
 						}
 					}
 
-					printMap();
+					//printMap();
 				}
 
 			} catch (Exception e) {
